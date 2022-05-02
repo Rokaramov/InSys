2eb17a01de1b1e1fc3f1e7dfb514625ce9326d6f
pozid <pozid@LAPTOP-N9JHCGGP>
2022-05-02 12:11:40 +0500

    dev3 изменил класс Registration

--- src/main/java/packReg/Registration.java
+++ src/main/java/packReg/Registration.java
@@ -1,5 +1,71 @@
 package packReg;
 
-public class Registration {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name="Registration", urlPatterns="/Registration")
+
+public class Registration extends HttpServlet{
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestRegistration Registration = RequestRegistration.fromRequestParameters(request);
+		Registration.setAsRequestAttributesAndPaste(request);
+		
+		request.getRequestDispatcher("/Avtoriz.jsp").forward(request, response);
+	}
+	private static class RequestRegistration {
+		private final String loginJava;
+		private final String passwordJava;
+		
+		private RequestRegistration (String loginReg, String passwordReg) {
+			this.loginJava = loginReg;
+			this.passwordJava = passwordReg;
+		}
+		
+public static RequestRegistration fromRequestParameters(HttpServletRequest request) {
+return new RequestRegistration(request.getParameter("loginReg"), request.getParameter("passwordReg"));
+}
+		
+		public void setAsRequestAttributesAndPaste(HttpServletRequest request) {
+
+			try {
+				request.setAttribute("loginReg", loginJava);
+				request.setAttribute("passwordReg", passwordJava);
+				
+				File file = new File("login1.TXT");
+				file.createNewFile();
+				FileWriter writer = new FileWriter(file, true);
+				writer.write(loginJava + "\n");
+				writer.close();
+				
+				File file2 = new File("password1.TXT");
+				file2.createNewFile();
+				FileWriter writerPassword = new FileWriter(file2, true);
+				writerPassword.write(passwordJava + "\n");
+				writerPassword.close();
+				
+				File file3 = new File("admin.TXT");
+				file3.createNewFile();
+				FileWriter writerAdmin = new FileWriter(file3, true);
+				if (request.getParameter("gender").equals("admin")) {
+					writerAdmin.write("1" + "\n");
+					writerAdmin.close();
+				} else {
+					writerAdmin.write("0" + "\n");
+					writerAdmin.close();
+				}					
+				request.setAttribute("log", loginJava);
+				request.setAttribute("pass", passwordJava);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				request.setAttribute("log", "Регистрация не совершена");		}	
+		}
+	}
 }
