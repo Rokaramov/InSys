0ec4f2d2e0f828fc71b0ed3739f3632632cd9e15
karamov <xchks@mail.ru>
2022-05-05 19:52:45 +0500

    tests

---
+++ src/test/java/kurTest/PercentTest.java
@@ -0,0 +1,22 @@
+package kurTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import packCalc.Out;
+
+public class PercentTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		Out ot = new Out(11100,0,12,13);
+	}
+
+	@Test
+	public void test() {
+		
+	}
+}
