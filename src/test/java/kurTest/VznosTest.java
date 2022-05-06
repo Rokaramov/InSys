ab4ef47954822145731ad3a31deff5cfc9f12c24
Baikov <baykoff.b@gmail.com>
2022-05-06 23:43:41 +0400

    Байков создал тест ввода первого взноса

--- src/test/java/kurTest/VznosTest.java
+++ src/test/java/kurTest/VznosTest.java
@@ -2,23 +2,26 @@ package kurTest;
 
 import static org.junit.Assert.*;
 
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import packCalc.Abst;
+import packCalc.Out;
+
 public class VznosTest {
 
+	private static double check;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+		Out out = new Out(3000000,12,13,0);
+		Abst abst;
+		abst = out;
+		check = abst.calc();
 	}
 
 	@Test
 	public void test() {
-		//fail("Not yet implemented");
+		assertNotNull(check);
 	}
-
 }
