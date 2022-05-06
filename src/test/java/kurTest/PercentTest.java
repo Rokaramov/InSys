a30285d6e5f0be3e65a8144dad995bbe52919333
ismagilova <ismagvika@gmail.com>
2022-05-07 00:49:02 +0400

    Создала тест проверка ввода 0 в поле процентов

--- src/test/java/kurTest/PercentTest.java
+++ src/test/java/kurTest/PercentTest.java
@@ -6,19 +6,25 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import packCalc.Abst;
+import packCalc.Out;
+
 public class PercentTest {
+
+	private static double check;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		Out out = new Out(30000000,0,13,12);
+		Abst abst;
+		abst = out;
+		check = abst.calc();
 	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
 
 	@Test
 	public void test() {
-		//fail("Not yet implemented");
+		assertNotNull(check);
 	}
 
 }
